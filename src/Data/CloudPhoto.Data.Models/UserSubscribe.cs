e9d002fcefb4ca315b332036ab8f9fff31f7676c
chemiliq-git <chemiliq@gmail.com>
2020-12-17 10:06:09 +0200

    Add UserSubscribe.cs

---
+++ src/Data/CloudPhoto.Data.Models/UserSubscribe.cs
@@ -0,0 +1,25 @@
+﻿namespace CloudPhoto.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using CloudPhoto.Data.Common.Models;
+
+    public class UserSubscribe : BaseDeletableModel<string>
+    {
+        public UserSubscribe()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string UserSubscribedId { get; set; }
+
+        [Required]
+        public string SubscribeToUserId { get; set; }
+
+        public virtual ApplicationUser UserSubscribed { get; set; }
+
+        public virtual ApplicationUser SubscribeToUser { get; set; }
+    }
+}
