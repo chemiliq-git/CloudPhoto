cbc426be35b8bf20077c8d6f76aaeb520ef6b6f4
chemiliq-git <chemiliq@gmail.com>
2020-12-29 16:00:03 +0200

    Add new file relation by preview commit
    1. ImageService: Migrate from Dapper to LINQ
    2. Remove tagService case-sensitive test, not work into real DB

---
+++ src/Services/CloudPhoto.Services.Data/ImagiesService/ResponseSearchImageModelData.cs
@@ -0,0 +1,29 @@
+﻿namespace CloudPhoto.Services.Data.ImagiesService
+{
+    using CloudPhoto.Data.Models;
+
+    public class ResponseSearchImageModelData
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ThumbnailImageUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageType { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorAvatarUrl { get; set; }
+
+        public bool IsFollow { get; set; }
+
+        public bool IsLike { get; set; }
+
+        public int LikeCount { get; set; }
+    }
+}
